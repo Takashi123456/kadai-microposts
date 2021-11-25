fa9394429b7c6f0e36464e822962ea948acae90e
takashi.sato <weshop2wen@gmail.com>
2021-11-25 02:46:49 +0000

    user signup

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,16 @@
+class UsersController < ApplicationController
+  def index
+    @pagy, @users = pagy(User.order(id: :desc), items:25)
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @users = User.new
+  end
+
+  def create
+  end
+end
